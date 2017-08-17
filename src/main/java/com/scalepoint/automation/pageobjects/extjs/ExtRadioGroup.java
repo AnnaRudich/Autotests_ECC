db8ff518676065e423d71b5cf2e7b8e3dc579447
Bartlomiej Zawadzki <bza@scalepoint.com>
2017-08-17 17:17:49 +0200

    Fixes for chrome

--- src/main/java/com/scalepoint/automation/pageobjects/extjs/ExtRadioGroup.java
+++ src/main/java/com/scalepoint/automation/pageobjects/extjs/ExtRadioGroup.java
@@ -1,20 +1,32 @@
 package com.scalepoint.automation.pageobjects.extjs;
 
+import com.scalepoint.automation.Actions;
 import com.scalepoint.automation.utils.threadlocal.Browser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class ExtRadioGroup extends ExtElement {
+public class ExtRadioGroup extends ExtElement implements Actions{
+
+    protected Logger logger = LogManager.getLogger(ExtRadioGroup.class);
 
     public ExtRadioGroup(WebElement wrappedElement) {
         super(wrappedElement);
     }
 
     public void select(int index) {
-        getWrappedElement().findElements(By.tagName("input")).get(index).click();
+        WebElement element =  getWrappedElement().findElements(By.tagName("input")).get(index);
+        try {
+            element.click();
+        }catch (Exception e){
+            logger.error(e.getMessage());
+            logger.info("Using JS click for element");
+            ClickElementUsingJS(element);
+        }
     }
 
     public void select() {
