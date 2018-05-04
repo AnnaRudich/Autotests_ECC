5bc3ef9ad4417e272e45f21a79e9c11ca6801024
Bartlomiej Zawadzki <bza@scalepoint.com>
2018-05-04 15:32:37 +0200

    CONTENT-2914 more tests

--- src/main/java/com/scalepoint/automation/pageobjects/pages/ClaimSearchPage.java
+++ src/main/java/com/scalepoint/automation/pageobjects/pages/ClaimSearchPage.java
@@ -17,172 +17,215 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClaimSearchPage extends Page {
 
-  @Override
-  protected Page ensureWeAreOnPage() {
-    return null;
-  }
-
-  @Override
-  protected String getRelativeUrl() {
-    return null;
-  }
-
-  @FindBy(id = "namefield")
-  private WebElement namefieldInput;
-
-  @FindBy(id = "customernumberfield")
-  private WebElement customernumberfieldInput;
-
-  @FindBy(id = "claimnofield")
-  private WebElement claimnofieldInput;
-
-  @FindBy(id = "phonefield")
-  private WebElement phonefieldInput;
-
-  @FindBy(id = "settlementdatefield")
-  private WebElement settlementdatefieldInput;
-
-  @FindBy(id = "claimhandler")
-  private WebElement claimhandlerInput;
-
-  @FindBy(id = "addressfield")
-  private WebElement addressfieldInput;
-
-  @FindBy(id = "company")
-  private Select companySelect;
-
-  @FindBy(id = "claimstate")
-  private Select claimstateSelect;
-
-  @FindBy(id = "postalfield")
-  private WebElement postalfieldInput;
-
-  @FindBy(id = "soeg")
-  private WebElement soegButton;
-
-
-  public List<ClaimRow> getClaimRows(){
-    return $(".claim-search-table").findAll("tr")
-            .stream().skip(1).map(ClaimRow::new).collect(Collectors.toList());
-  }
-
-  public ClaimSearchPage fillClaimNumber(String claimNumber) {
-    claimnofieldInput.clear();
-    claimnofieldInput.sendKeys(claimNumber);
-    return this;
-  }
-
-  public ClaimSearchPage search() {
-    soegButton.click();
-    Wait.waitForAjaxCompleted();
-    return this;
-  }
-
-  public static class ClaimRow {
-
-    private String name;
-    private String claimNumber;
-    private String claimHandler;
-    private String company;
-    private String claimState;
-    private String settlementDate;
-    private String activeDate;
-
-    ClaimRow(WebElement element) {
-      this.name = element.findElement(By.xpath("./td[1]//label")).getText();
-      this.claimNumber = element.findElement(By.xpath("./td[2]")).getText();
-      this.claimHandler = element.findElement(By.xpath("./td[3]")).getText();
-      this.company = element.findElement(By.xpath("./td[4]")).getText();
-      this.claimState = element.findElement(By.xpath("./td[5]")).getText();
-      this.settlementDate = element.findElement(By.xpath("./td[6]")).getText();
-      this.activeDate = element.findElement(By.xpath("./td[7]")).getText();
+    @Override
+    protected Page ensureWeAreOnPage() {
+        return null;
     }
 
-    public String getName() {
-      return name;
+    @Override
+    protected String getRelativeUrl() {
+        return null;
     }
 
-    public String getClaimNumber() {
-      return claimNumber;
+    @FindBy(id = "namefield")
+    private WebElement namefieldInput;
+
+    @FindBy(id = "customernumberfield")
+    private WebElement customernumberfieldInput;
+
+    @FindBy(id = "claimnofield")
+    private WebElement claimnofieldInput;
+
+    @FindBy(id = "phonefield")
+    private WebElement phonefieldInput;
+
+    @FindBy(id = "settlementdatefield")
+    private WebElement settlementdatefieldInput;
+
+    @FindBy(id = "claimhandler")
+    private WebElement claimhandlerInput;
+
+    @FindBy(id = "addressfield")
+    private WebElement addressfieldInput;
+
+    @FindBy(id = "company")
+    private WebElement companySelect;
+
+    @FindBy(id = "claimstate")
+    private WebElement claimstateSelect;
+
+    @FindBy(id = "postalfield")
+    private WebElement postalfieldInput;
+
+    @FindBy(id = "soeg")
+    private WebElement soegButton;
+
+
+    public List<ClaimRow> getClaimRows() {
+        return $(".claim-search-table").findAll("tr")
+                .stream().skip(1).map(ClaimRow::new).collect(Collectors.toList());
     }
 
-    public String getClaimHandler() {
-      return claimHandler;
+    public ClaimSearchPage fillClaimNumber(String claimNumber) {
+        claimnofieldInput.clear();
+        claimnofieldInput.sendKeys(claimNumber);
+        return this;
     }
 
-    public String getCompany() {
-      return company;
+    public ClaimSearchPage fillCustomerNumber(String customerNumber) {
+        customernumberfieldInput.clear();
+        customernumberfieldInput.sendKeys(customerNumber);
+        return this;
     }
 
-    public String getClaimState() {
-      return claimState;
+    public ClaimSearchPage fillCompany(String company) {
+        new Select(companySelect).selectByVisibleText(company);
+        return this;
     }
 
-    public String getSettlementDate() {
-      return settlementDate;
+    public ClaimSearchPage fillCustomerName(String customerName) {
+        namefieldInput.clear();
+        namefieldInput.sendKeys(customerName);
+        return this;
     }
 
-    public String getActiveDate() {
-      return activeDate;
-    }
-  }
-
-  public ClaimSearchPage doAssert(Claim claim, Consumer<ClaimSearchPage.Asserts> assertsFunc) {
-    assertsFunc.accept(new ClaimSearchPage.Asserts(claim));
-    return ClaimSearchPage.this;
-  }
-
-  public ClaimSearchPage doAssert(Consumer<ClaimSearchPage.Asserts> assertsFunc) {
-    return doAssert(null, assertsFunc);
-  }
-
-  public class Asserts {
-
-    private Claim claim;
-    private List<ClaimRow> claimRows;
-
-    public Asserts(Claim claim){
-      this.claim = claim;
-      this.claimRows = getClaimRows();
+    public ClaimSearchPage search() {
+        soegButton.click();
+        Wait.waitForAjaxCompleted();
+        return this;
     }
 
-    public Asserts isOnlyOnList(){
-      assertThat(claimRows.stream().allMatch(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber()))).isTrue();
-      return this;
+    public ClaimSearchPage fillClaimHandler(String claimHandler) {
+        claimhandlerInput.clear();
+        claimhandlerInput.sendKeys(claimHandler);
+        return this;
     }
 
-    public Asserts isClaimState(ClaimState state){
-      assertThat(claimRows.stream().filter(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber())).findFirst().get().getClaimState())
-              .isEqualToIgnoringCase(state.getStateFullText());
-      return this;
+    public static class ClaimRow {
+
+        private String name;
+        private String claimNumber;
+        private String claimHandler;
+        private String company;
+        private String claimState;
+        private String settlementDate;
+        private String activeDate;
+
+        ClaimRow(WebElement element) {
+            this.name = element.findElement(By.xpath("./td[1]//label")).getText();
+            this.claimNumber = element.findElement(By.xpath("./td[2]")).getText();
+            this.claimHandler = element.findElement(By.xpath("./td[3]")).getText();
+            this.company = element.findElement(By.xpath("./td[4]")).getText();
+            this.claimState = element.findElement(By.xpath("./td[5]")).getText();
+            this.settlementDate = element.findElement(By.xpath("./td[6]")).getText();
+            this.activeDate = element.findElement(By.xpath("./td[7]")).getText();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getClaimNumber() {
+            return claimNumber;
+        }
+
+        public String getClaimHandler() {
+            return claimHandler;
+        }
+
+        public String getCompany() {
+            return company;
+        }
+
+        public String getClaimState() {
+            return claimState;
+        }
+
+        public String getSettlementDate() {
+            return settlementDate;
+        }
+
+        public String getActiveDate() {
+            return activeDate;
+        }
     }
 
-    public Asserts isClaimCompany(String companyName){
-      assertThat(claimRows.stream().filter(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber())).findFirst().get().getCompany())
-              .isEqualToIgnoringCase(companyName);
-      return this;
-    }
-  }
-
-  public enum ClaimState {
-
-    IN_USE('W'),
-    DELETED('D'),
-    OPEN('P'),
-    COMPLETED('S'),
-    LOCKED_BY_ORDER('L'),
-    CANCELED('X'),
-    CLOSED_EXTERNALLY('E');
-
-    private Character state;
-
-    ClaimState(Character state){
-      this.state = state;
+    public ClaimSearchPage doAssert(Claim claim, Consumer<ClaimSearchPage.Asserts> assertsFunc) {
+        assertsFunc.accept(new ClaimSearchPage.Asserts(claim));
+        return ClaimSearchPage.this;
     }
 
-    public String getStateFullText(){
-      return TestData.getClaimStates().get(this.state);
+    public ClaimSearchPage doAssert(Consumer<ClaimSearchPage.Asserts> assertsFunc) {
+        return doAssert(null, assertsFunc);
     }
-  }
+
+    public class Asserts {
+
+        private Claim claim;
+        private List<ClaimRow> claimRows;
+
+        public Asserts(Claim claim) {
+            this.claim = claim;
+            this.claimRows = getClaimRows();
+        }
+
+        public Asserts isOnlyOnList() {
+            assertThat(claimRows.stream().allMatch(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber()))).isTrue();
+            return this;
+        }
+
+        public Asserts isClaimState(ClaimState state) {
+            assertThat(claimRows.stream().filter(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber())).findFirst().get().getClaimState())
+                    .isEqualToIgnoringCase(state.getStateFullText());
+            return this;
+        }
+
+        public Asserts isClaimCompany(String companyName) {
+            assertThat(claimRows.stream().filter(claimRow -> claimRow.getClaimNumber().equals(this.claim.getClaimNumber())).findFirst().get().getCompany())
+                    .isEqualToIgnoringCase(companyName);
+            return this;
+        }
+
+        public Asserts areClaimsMatchingCompany(String companyName) {
+            assertThat(claimRows.stream().map(ClaimRow::getCompany)).allMatch(row -> row.equals(companyName));
+            return this;
+        }
+
+        public Asserts areClaimsMatchingName() {
+            assertThat(claimRows.stream().map(ClaimRow::getName)).allMatch(row -> row.contains(claim.getFirstName()));
+            return this;
+        }
+
+        public Asserts areClaimsMatchingLastName() {
+            assertThat(claimRows.stream().map(ClaimRow::getName)).allMatch(row -> row.contains(claim.getLastName()));
+            return this;
+        }
+
+        public Asserts areClaimsMatchingClaimHandler(String claimHandler) {
+            assertThat(claimRows.stream().map(ClaimRow::getClaimHandler)).allMatch(row -> row.equals(claimHandler));
+            return this;
+        }
+    }
+
+    public enum ClaimState {
+
+        IN_USE('W'),
+        DELETED('D'),
+        OPEN('P'),
+        COMPLETED('S'),
+        LOCKED_BY_ORDER('L'),
+        CANCELED('X'),
+        CLOSED_EXTERNALLY('E');
+
+        private Character state;
+
+        ClaimState(Character state) {
+            this.state = state;
+        }
+
+        public String getStateFullText() {
+            return TestData.getClaimStates().get(this.state);
+        }
+    }
 
 }
--- src/main/java/com/scalepoint/automation/tests/search/ClaimSearchTest.java
+++ src/main/java/com/scalepoint/automation/tests/search/ClaimSearchTest.java
@@ -1,19 +1,60 @@
 package com.scalepoint.automation.tests.search;
 
+import com.scalepoint.automation.pageobjects.pages.ClaimSearchPage;
+import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
 import com.scalepoint.automation.tests.BaseTest;
 import com.scalepoint.automation.utils.annotations.RunOn;
+import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.Claim;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
 import com.scalepoint.automation.utils.driver.DriverType;
 import org.testng.annotations.Test;
 
+import static com.scalepoint.automation.pageobjects.pages.ClaimSearchPage.ClaimState.CLOSED_EXTERNALLY;
+import static com.scalepoint.automation.pageobjects.pages.ClaimSearchPage.ClaimState.COMPLETED;
 import static com.scalepoint.automation.pageobjects.pages.ClaimSearchPage.ClaimState.IN_USE;
+import static com.scalepoint.automation.pageobjects.pages.ClaimSearchPage.ClaimState.OPEN;
 
+@RunOn(DriverType.CHROME)
 public class ClaimSearchTest extends BaseTest {
 
-    @RunOn(DriverType.CHROME)
     @Test(dataProvider = "testDataProvider", description = "Search for claim")
-    public void searchClaim(User user, Claim claim) {
+    public void searchClaim_claimHandler(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .getMainMenu()
+                .openClaimSearch()
+                .fillClaimHandler("FirstName LastName")
+                .search()
+                .doAssert(asserts -> asserts.areClaimsMatchingClaimHandler("FirstName LastName"));
+    }
+
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_byName(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .getMainMenu()
+                .openClaimSearch()
+                .fillCustomerName(claim.getFirstName())
+                .search()
+                .doAssert(claim, ClaimSearchPage.Asserts::areClaimsMatchingName)
+                .fillCustomerName(claim.getLastName())
+                .search()
+                .doAssert(claim, ClaimSearchPage.Asserts::areClaimsMatchingName);
+    }
+
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_byCompany(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .getMainMenu()
+                .openClaimSearch()
+                .fillCompany(user.getCompanyName())
+                .search()
+                .doAssert(asserts -> {
+                    asserts.areClaimsMatchingCompany(user.getCompanyName());
+                });
+    }
+
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_inUse(User user, Claim claim) {
         loginAndCreateClaim(user, claim)
                 .getMainMenu()
                 .openClaimSearch()
@@ -22,6 +63,55 @@ public class ClaimSearchTest extends BaseTest {
                 .doAssert(claim, asserts -> {
                     asserts.isOnlyOnList();
                     asserts.isClaimState(IN_USE);
+                    asserts.isClaimCompany(user.getCompanyName());
+                });
+    }
+
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_open(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .saveClaim()
+                .getMainMenu()
+                .openClaimSearch()
+                .fillClaimNumber(claim.getClaimNumber())
+                .search()
+                .doAssert(claim, asserts -> {
+                    asserts.isOnlyOnList();
+                    asserts.isClaimState(OPEN);
+                    asserts.isClaimCompany(user.getCompanyName());
+                });
+    }
+
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_completed(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .toCompleteClaimPage()
+                .fillClaimForm(claim)
+                .completeWithEmail()
+                .getMainMenu()
+                .openClaimSearch()
+                .fillClaimNumber(claim.getClaimNumber())
+                .search()
+                .doAssert(claim, asserts -> {
+                    asserts.isOnlyOnList();
+                    asserts.isClaimState(COMPLETED);
+                    asserts.isClaimCompany(user.getCompanyName());
+                });
+    }
+
+    @RequiredSetting(type = FTSetting.ENABLE_SETTLE_EXTERNALLY_BUTTON_IN_SETTLEMENT_PAGE)
+    @RequiredSetting(type = FTSetting.SETTLE_WITHOUT_MAIL)
+    @Test(dataProvider = "testDataProvider", description = "Search for claim")
+    public void searchClaim_closedExternally(User user, Claim claim) {
+        loginAndCreateClaim(user, claim)
+                .completeClaimWithoutMail()
+                .getMainMenu()
+                .openClaimSearch()
+                .fillClaimNumber(claim.getClaimNumber())
+                .search()
+                .doAssert(claim, asserts -> {
+                    asserts.isOnlyOnList();
+                    asserts.isClaimState(CLOSED_EXTERNALLY);
                     asserts.isClaimCompany(user.getCompanyName());
                 });
     }
