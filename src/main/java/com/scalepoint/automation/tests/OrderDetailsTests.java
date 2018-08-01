3b0caee74b5de2d775f645836fa9c59a32623aec
bza <bza@scalepoint.com>
2018-08-01 09:53:57 +0200

    Removed unused imports

--- src/main/java/com/scalepoint/automation/tests/OrderDetailsTests.java
+++ src/main/java/com/scalepoint/automation/tests/OrderDetailsTests.java
@@ -8,7 +8,6 @@ import com.scalepoint.automation.services.externalapi.SolrApi;
 import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
 import com.scalepoint.automation.shared.ProductInfo;
 import com.scalepoint.automation.utils.annotations.Jira;
-import com.scalepoint.automation.utils.annotations.RunOn;
 import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.Claim;
 import com.scalepoint.automation.utils.data.entity.ClaimItem;
@@ -16,7 +15,6 @@ import com.scalepoint.automation.utils.data.entity.OrderDetails;
 import com.scalepoint.automation.utils.data.entity.TextSearch;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
 import com.scalepoint.automation.utils.data.entity.payments.Payments;
-import com.scalepoint.automation.utils.driver.DriverType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
