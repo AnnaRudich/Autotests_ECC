679c14488b7f5614c6a132431e6023ee1153de00
Anna Rudych <aru@scalepoint.com>
2018-08-02 14:34:40 +0200

    adding javadoc comment to the new annotation

--- src/main/java/com/scalepoint/automation/utils/annotations/FtSettingsByDefault.java
+++ src/main/java/com/scalepoint/automation/utils/annotations/FtSettingsByDefault.java
@@ -1,7 +1,10 @@
 package com.scalepoint.automation.utils.annotations;
 
 import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
-
+/**
+ * this annotation is used only as an info note
+ * it has a list of FT settings which are expected to be set by default
+ */
 public @interface FtSettingsByDefault {
     String UNDEFINED = "undefined";
 
