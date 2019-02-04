93d20b2a030bfb5a62ad60114a7352290bec35ca
Alexander Tuz <atu@scalepoint.com>
2019-02-04 19:02:48 +0200

    CONTENTS-4104 Cover Unified Payments solution with Unit tests

--- src/main/java/com/scalepoint/automation/tests/api/unifiedpayments/SendingCaseSettledEventV3Case3.java
+++ src/main/java/com/scalepoint/automation/tests/api/unifiedpayments/SendingCaseSettledEventV3Case3.java
@@ -59,10 +59,12 @@ public class SendingCaseSettledEventV3Case3 extends BaseUnifiedPaymentsApiTest {
     @Test(dataProvider = "testDataProvider", dataProviderClass = BaseTest.class)
     public void closeExternally(User user, InsertSettlementItem item1, InsertSettlementItem item2, InsertSettlementItem item3, InsertSettlementItem item4) {
         //GIVEN
-        //1st item with price 1000 and depreciation  500    (50%)
-        //2nd item with price 100  and depreciation  0      (0%)
-        //3rd item with price 500  and depreciation  100    (20%)
-        //4th item with price 500  and depreciation  100    (20%)
+        /*
+            1st item with price 1000 and depreciation  500    (50%)
+            2nd item with price 100  and depreciation  0      (0%)
+            3rd item with price 500  and depreciation  100    (20%)
+            4th item with price 500  and depreciation  100    (20%)
+        */
 
 
         //WHEN----------------------------------------------------------------------------------------------------------
@@ -144,10 +146,12 @@ public class SendingCaseSettledEventV3Case3 extends BaseUnifiedPaymentsApiTest {
 
     private void close(User user, InsertSettlementItem item1, InsertSettlementItem item2, InsertSettlementItem item3, InsertSettlementItem item4, SettlementClaimService.CloseCaseReason closeCaseReason) {
         //GIVEN
-        //1st item with price 1000 and depreciation 500    (50%)
-        //2nd item with price 100  and depreciation 0      (0%)
-        //3rd item with price 500  and depreciation 100    (20%)
-        //4th item with price 500  and depreciation 100    (20%)
+        /*
+            1st item with price 1000 and depreciation 500    (50%)
+            2nd item with price 100  and depreciation 0      (0%)
+            3rd item with price 500  and depreciation 100    (20%)
+            4th item with price 500  and depreciation 100    (20%)
+        */
 
 
         //WHEN----------------------------------------------------------------------------------------------------------
--- src/main/java/com/scalepoint/automation/tests/api/unifiedpayments/SendingCaseSettledEventV3Case4_1.java
+++ src/main/java/com/scalepoint/automation/tests/api/unifiedpayments/SendingCaseSettledEventV3Case4_1.java
@@ -36,8 +36,10 @@ public class SendingCaseSettledEventV3Case4_1 extends SendingCaseSettledEventV3C
     @Test(dataProvider = "testDataProvider", dataProviderClass = BaseTest.class)
     private void closeExternally(User user, InsertSettlementItem item1, InsertSettlementItem item2, InsertSettlementItem item3) {
         //GIVEN
-        //1st item with price 3000 and depreciation  600    (20%)
-        //2nd item with price 2000 and depreciation  0      (0%)
+        /*
+            1st item with price 3000 and depreciation  600    (20%)
+            2nd item with price 2000 and depreciation  0      (0%)
+        */
 
 
         //WHEN
@@ -115,8 +117,10 @@ public class SendingCaseSettledEventV3Case4_1 extends SendingCaseSettledEventV3C
 
     private void close(User user, InsertSettlementItem item1, InsertSettlementItem item2, InsertSettlementItem item3, SettlementClaimService.CloseCaseReason closeCaseReason) {
         //GIVEN
-        //1st item with price 3000 and depreciation  600    (20%)
-        //2nd item with price 2000 and depreciation  0      (0%)
+        /*
+            1st item with price 3000 and depreciation  600    (20%)
+            2nd item with price 2000 and depreciation  0      (0%)
+        */
 
 
         //WHEN
