0eb537b285bb2b94b7b3fb91a84f8b94d03e787f
Anna Rudich <aru@scalepoint.com>
2019-06-12 12:01:33 +0200

    initial commit

--- src/main/java/com/scalepoint/automation/tests/rnv/RnVSmokeTest.java
+++ src/main/java/com/scalepoint/automation/tests/rnv/RnVSmokeTest.java
@@ -6,11 +6,13 @@ import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
 import com.scalepoint.automation.services.restService.RnvService;
 import com.scalepoint.automation.tests.BaseTest;
 import com.scalepoint.automation.utils.RandomUtils;
+import com.scalepoint.automation.utils.annotations.RunOn;
 import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.Claim;
 import com.scalepoint.automation.utils.data.entity.ServiceAgreement;
 import com.scalepoint.automation.utils.data.entity.Translations;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
+import com.scalepoint.automation.utils.driver.DriverType;
 import org.testng.annotations.Test;
 
 @RequiredSetting(type = FTSetting.ENABLE_DAMAGE_TYPE, enabled = false)
@@ -75,6 +77,41 @@ public class RnVSmokeTest extends BaseTest {
                 .toCommunicationTab()
                 .sendTextMailToSePa(testMessage)
                 .assertLatestMessageContains(testMessage);
+    }
+
+    @RunOn(DriverType.CHROME)
+    @RequiredSetting(type = FTSetting.ENABLE_DAMAGE_TYPE)
+    @RequiredSetting(type = FTSetting.ENABLE_REPAIR_VALUATION_AUTO_SETTLING, enabled = false)
+    @Test(dataProvider = "testDataProvider", description = "RnV1. SendLine to RnV, send Service Partner feedback")
+    public void sendLineToRnv_damageType(User user, Claim claim, ServiceAgreement agreement, Translations translations) {
+
+        String lineDescription = RandomUtils.randomName("RnVLine");
+
+        loginAndCreateClaim(user, claim)
+                .toCompleteClaimPage()
+                .fillClaimForm(claim)
+                .completeWithEmail(claim)
+                .openRecentClaim()
+                .reopenClaim()
+                .openSid()
+                .fill(lineDescription, agreement.getClaimLineCat_PersonligPleje(), agreement.getClaimLineSubCat_Medicin(), 100.00)
+                .closeSidWithOk()
+                .findClaimLine(lineDescription)
+                .selectLine()
+                .sendToRnV()
+                .changeTask(lineDescription, translations.getRnvTaskType().getRepair())
+                .nextRnVstep()
+                .sendRnV(agreement)
+
+                .findClaimLine(lineDescription)
+                .doAssert(SettlementPage.ClaimLine.Asserts::assertLineIsSentToRepair);
+
+        new RnvService().sendDefaultFeedback(claim);
+
+        new ClaimNavigationMenu().toRepairValuationProjectsPage()
+                .expandTopTaskDetails()
+                .getAssertion()
+                .assertTaskHasFeedbackReceivedStatus(agreement);
     }
 }
 
