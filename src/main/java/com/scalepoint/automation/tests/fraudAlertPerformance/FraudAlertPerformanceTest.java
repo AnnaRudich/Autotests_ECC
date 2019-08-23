6d62cc6f79d2b9ded4a2f4f7eaf1b2a10aa11a71
Bartosz Najtkowski <bna@scalepoint.com>
2019-08-23 08:41:45 +0200

    fixes after merge

--- src/main/java/com/scalepoint/automation/tests/fraudAlertPerformance/FraudAlertPerformanceTest.java
+++ src/main/java/com/scalepoint/automation/tests/fraudAlertPerformance/FraudAlertPerformanceTest.java
@@ -10,10 +10,9 @@ import com.scalepoint.automation.tests.api.BaseApiTest;
 import com.scalepoint.automation.utils.data.TestData;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
 import com.scalepoint.automation.utils.data.entity.eventsApiEntity.changed.Case;
-import com.scalepoint.automation.utils.data.entity.eventsApiEntity.changed.Item;
+import com.scalepoint.automation.utils.data.entity.eventsApiEntity.fraudStatus.ClaimLineChanged;
 import com.scalepoint.automation.utils.data.request.ClaimRequest;
 import com.scalepoint.automation.utils.data.request.InsertSettlementItem;
-import com.scalepoint.automation.utils.data.response.Token;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -22,9 +21,9 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.temporal.TemporalUnit;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FraudAlertPerformanceTest extends BaseApiTest {
 
@@ -46,21 +45,18 @@ public class FraudAlertPerformanceTest extends BaseApiTest {
     }
 
     @Test(dataProvider = "usersDataProvider")
-    public void testAdd(User user) throws IOException {
-
+    public void testAdd(User user) throws InterruptedException {
         ClaimRequest claimRequest = TestData.getClaimRequestFraudAlert();
 
-
+        Thread.sleep(Thread.currentThread().getId() * 10);
         ClaimSettlementItemsService claimSettlementItemsService = BaseService.loginAndOpenClaimWithItems(user, claimRequest, TestData.getInsertSettlementItem());
         LocalDateTime start = LocalDateTime.now();
         String token = claimSettlementItemsService.getData().getClaimToken();
-                fraudAlertStubs
+        List<ClaimLineChanged> events = fraudAlertStubs
                 .waitForClaimUpdatedEvents(token, 1);
 
-        Case caseChanged = new UnifiedIntegrationService()
-                .getCaseEndpointByToken(COUNTRY, TENANT, token);
+        new EventApiService().sendFraudStatus(events.get(0), "FRAUDULENT");
 
-        new EventApiService().sendFraudStatus(caseChanged, "FRAUDULENT");
         databaseApi.waitForFraudStatusChange(1, claimRequest.getCaseNumber());
         LocalDateTime end = LocalDateTime.now();
 
@@ -68,36 +64,42 @@ public class FraudAlertPerformanceTest extends BaseApiTest {
         log.info("Duration: {}", duration);
     }
 
-    @Test(enabled = false)
-    public void testRemove() {
+    @Test(dataProvider = "usersDataProvider", enabled = false)
+    public void testRemove(User user) throws InterruptedException {
 
-        TestData.getInsertSettlementItem();
-        User user = TestData
-                .getSystemUsers()
-                .getUsers()
-                .stream()
-                .filter(u -> u.getCompanyCode() != null)
-                .filter(u -> u.getCompanyCode().equals("TOPDANMARK"))
-                .findFirst()
-                .get();
+        ClaimRequest claimRequest = TestData.getClaimRequestFraudAlert();
 
         InsertSettlementItem insertSettlementItem = TestData.getInsertSettlementItem();
 
+        Thread.sleep(Thread.currentThread().getId() * 100);
         ClaimSettlementItemsService claimSettlementItemsService = BaseService
-                .loginAndOpenClaimWithItems(user, TestData.getClaimRequestFraudAlert(), insertSettlementItem)
+                .loginAndOpenClaimWithItems(user, claimRequest, insertSettlementItem)
                 .removeLines(insertSettlementItem);
+
+        LocalDateTime start = LocalDateTime.now();
+        String token = claimSettlementItemsService.getData().getClaimToken();
+        List<ClaimLineChanged> events = fraudAlertStubs
+                .waitForClaimUpdatedEvents(token, 2);
+
+        new EventApiService().sendFraudStatus(events.get(1), "FRAUDULENT");
+        databaseApi.waitForFraudStatusChange(1, claimRequest.getCaseNumber());
+        LocalDateTime end = LocalDateTime.now();
+
+        long duration = Duration.between(start, end).getSeconds();
+        assertThat(duration).isLessThan(5);
+        log.info("Duration: {}", duration);
     }
 
     @DataProvider(name = "usersDataProvider", parallel = true)
     public static Object[][] usersDataProvider(Method method) {
 
-        int size = 10;
+        int size = 1;
 
         Object[][] objects = new Object[size][1];
 
         for(int i =0;i<size;i++){
 
-            objects[i][0] = new User("autotest-topdanmark".concat(new Integer(i+1).toString()), "12341234");
+            objects[i][0] = new User("autotest-topdanmark".concat(new Integer(i + 1).toString()), "12341234");
         }
 
         return objects;
