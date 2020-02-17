941ff3132ea586e8b626997bd3ee4d037f25d87c
Bartosz Najtkowski <bna@scalepoint.com>
2020-02-17 16:03:43 +0100

    fixes for filesService performance tests

--- src/main/java/com/scalepoint/automation/services/restService/FilesServiceService.java
+++ src/main/java/com/scalepoint/automation/services/restService/FilesServiceService.java
@@ -1,18 +1,25 @@
 package com.scalepoint.automation.services.restService;
 
 import com.scalepoint.automation.services.restService.Common.BaseService;
-import com.scalepoint.automation.utils.data.request.ClaimRequest;
 import com.scalepoint.automation.utils.data.response.Token;
 import io.restassured.RestAssured;
 import io.restassured.config.HttpClientConfig;
 import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.SystemDefaultHttpClient;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.HttpClientConfig.httpClientConfig;
+import static io.restassured.config.RestAssuredConfig.newConfig;
 
 public class FilesServiceService extends BaseService {
 
@@ -34,19 +41,19 @@ public class FilesServiceService extends BaseService {
 
     public FilesServiceService getFile(String fileGUID) {
 
-        RestAssuredConfig config = RestAssured.config()
-                .httpClient(HttpClientConfig.httpClientConfig()
-                        .setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 60000)
-                        .setParam(CoreConnectionPNames.SO_TIMEOUT, 60000));
-
         LocalDateTime start = LocalDateTime.now();
-        this.response = given().config(config)
+        this.response = given()
                 .baseUri("https://dev-ecc-01.spcph.test")
                 .basePath("/api/fs/files/{locale}/{tenant}/{fileGUID}")
-//                .log().all()
+                .log().all()
                 .pathParam("locale", "dk")
                 .pathParam("tenant", "topdanmark")
                 .pathParam("fileGUID", fileGUID)
+                .contentType(ContentType.JSON)
+                .accept("*/*")
+                .headers("Accept-Encoding", "gzip, deflate, br")
+                .headers("Cache-Control", "no-cache")
+                .headers("Connection", "keep-alive")
                 .header(token.getAuthorizationHeader())
                 .when()
                 .get()
