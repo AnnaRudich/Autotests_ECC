ec8c702960ef29d13dc84d328b00def48e61e8b4
Anna Rudich <aru@scalepoint.com>
2020-03-23 18:50:19 +0100

    disabling the test which failing because of a bug waiting to be fixed

--- src/main/java/com/scalepoint/automation/tests/eccCwaIntegration/EccIntegrationsWithCwaClaimSSTests.java
+++ src/main/java/com/scalepoint/automation/tests/eccCwaIntegration/EccIntegrationsWithCwaClaimSSTests.java
@@ -6,6 +6,7 @@ import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
 import com.scalepoint.automation.shared.CwaTaskLog;
 import com.scalepoint.automation.tests.BaseTest;
 import com.scalepoint.automation.utils.Constants;
+import com.scalepoint.automation.utils.annotations.Jira;
 import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.TestData;
 import com.scalepoint.automation.utils.data.entity.ClaimItem;
@@ -34,10 +35,10 @@ public class EccIntegrationsWithCwaClaimSSTests extends BaseTest {
         claimToken = createCwaClaimAndGetClaimToken(claimRequest);
         userIdByClaimToken = databaseApi.getUserIdByClaimToken(claimToken.replace("c.", ""));
     }
-
+    @Jira("https://jira.scalepoint.com/browse/CLAIMSHOP-5660")
     @RequiredSetting(type = FTSetting.USE_SELF_SERVICE2)
     @RequiredSetting(type = FTSetting.ENABLE_SELF_SERVICE)
-    @Test(dataProvider = "testDataProvider")
+    @Test(enabled = false, dataProvider = "testDataProvider")
     public void selfServiceImport(User user, ClaimItem claimItem) {
         SettlementPage settlementPage = loginAndOpenUnifiedIntegrationClaimByToken(user, claimToken)
                 .requestSelfService(Constants.DEFAULT_PASSWORD);
