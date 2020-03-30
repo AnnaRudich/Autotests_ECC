9c7ad580cf97b13dff55bfa41cac1d6c2b8bfa64
Anna Rudich <aru@scalepoint.com>
2020-03-30 19:50:14 +0200

    fix selector

--- src/main/java/com/scalepoint/automation/pageobjects/dialogs/RequiredValuationIsNeededDialog.java
+++ src/main/java/com/scalepoint/automation/pageobjects/dialogs/RequiredValuationIsNeededDialog.java
@@ -1,8 +1,6 @@
 package com.scalepoint.automation.pageobjects.dialogs;
 
 import com.codeborne.selenide.Condition;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -11,12 +9,9 @@ import static com.codeborne.selenide.Selenide.$;
  */
 public class RequiredValuationIsNeededDialog extends BaseDialog {
 
-    @FindBy(xpath = "//div[contains(@class,'required-valuation-is-needed-dialogs')]")
-    private WebElement dialog;
-
     @Override
     protected BaseDialog ensureWeAreAt() {
-        $(dialog).waitUntil(Condition.visible, TIME_OUT_IN_MILISECONDS);
+        $(".required-valuation-is-needed-dialog").waitUntil(Condition.visible, TIME_OUT_IN_MILISECONDS);
         return this;
     }
 
