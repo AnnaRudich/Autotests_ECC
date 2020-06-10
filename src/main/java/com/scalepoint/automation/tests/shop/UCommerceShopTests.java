a6b534a7b2a4bbc892c3df0a9f3ab9804260e6e1
Anna Rudich <aru@scalepoint.com>
2020-06-10 14:02:10 +0200

    adding toggle to manipulate JAXBUTILS use schemas, turning it off

--- src/main/java/com/scalepoint/automation/tests/shop/UCommerceShopTests.java
+++ src/main/java/com/scalepoint/automation/tests/shop/UCommerceShopTests.java
@@ -4,11 +4,13 @@ import com.scalepoint.automation.pageobjects.dialogs.SettlementDialog;
 import com.scalepoint.automation.pageobjects.pages.OrderDetailsPage;
 import com.scalepoint.automation.pageobjects.pages.SettlementPage;
 import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
+import com.scalepoint.automation.services.externalapi.ftoggle.FeatureIds;
 import com.scalepoint.automation.services.restService.CreateOrderService;
 import com.scalepoint.automation.shared.VoucherInfo;
 import com.scalepoint.automation.shared.XpriceInfo;
 import com.scalepoint.automation.tests.BaseTest;
 import com.scalepoint.automation.utils.Constants;
+import com.scalepoint.automation.utils.annotations.ftoggle.FeatureToggleSetting;
 import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.Claim;
 import com.scalepoint.automation.utils.data.entity.ClaimItem;
@@ -22,7 +24,7 @@ public class UCommerceShopTests extends BaseTest {
 
     private final Double orderedProductPrice = Constants.PRICE_100;
     private final Double orderedVoucherPrice = Constants.PRICE_100;
-
+    @FeatureToggleSetting(type = FeatureIds.JAXBUTILS_USE_SCHEMAS, enabled = false)
     @Test(dataProvider = "testDataProvider",
             description = "create order with product and verify orderTotals")
     public void orderProduct_positiveBalance(User user, Claim claim, ClaimItem claimItem) {
@@ -55,7 +57,7 @@ public class UCommerceShopTests extends BaseTest {
                         orderDetailsPage.assertCompensationAmount(activeValuation);
                 });
     }
-
+    @FeatureToggleSetting(type = FeatureIds.JAXBUTILS_USE_SCHEMAS, enabled = false)
     @Test(dataProvider = "testDataProvider",
             description = "create order with physical voucher and verify orderTotals")
     public void orderPhisicalVoucher(User user, Claim claim, ClaimItem claimItem) {
