7f8918864452ec8b4df3c53ff000969e717606d1
Anna Rudich <aru@scalepoint.com>
2020-09-29 14:07:05 +0200

    deleting not used code

--- src/main/java/com/scalepoint/automation/pageobjects/pages/oldshop/ShopFlow.java
+++ src/main/java/com/scalepoint/automation/pageobjects/pages/oldshop/ShopFlow.java
@@ -3,18 +3,9 @@ package com.scalepoint.automation.pageobjects.pages.oldshop;
 import com.scalepoint.automation.pageobjects.modules.oldshop.AccountBox;
 import com.scalepoint.automation.pageobjects.pages.CustomerDetailsPage;
 import com.scalepoint.automation.pageobjects.pages.Page;
-import com.scalepoint.automation.utils.data.entity.payments.Dankort;
 
 @SuppressWarnings("AccessStaticViaInstance")
 public abstract class ShopFlow extends Page {
-
-    public CustomerDetailsPage checkoutProduct() {
-        return getAccountBox().
-                toShoppingCart().
-                toOrderVerificationPage().
-                selectAgreeOption().
-                selectPlaceMyOrderOption();
-    }
 
     public CustomerDetailsPage checkoutProductWithdrawalWizard() {
         return submitWithdrawalWizard().
@@ -43,26 +34,6 @@ public abstract class ShopFlow extends Page {
                 selectPlaceMyOrderOption();
     }
 
-    public CustomerDetailsPage checkoutWithCreditCardWizard(Dankort dankort) {
-        return submitWithCreditCard(dankort).
-                acceptAndBackToEcc();
-    }
-
-    public OrderConfirmationPage checkoutWithCreditCardMail(Dankort dankort){
-        return submitWithCreditCard(dankort)
-                .accept();
-    }
-
-    public DibsAccept submitWithCreditCard(Dankort dankort){
-        return getAccountBox().
-                toShoppingCart().
-                toDepositPage().
-                selectCreditCard().
-                selectAgreeOption().
-                toDIBSPage().
-                selectDankortOption().
-                submitCardData(dankort.getNumber(), dankort.getExpMonth(), dankort.getExpYear(), dankort.getCvc());
-    }
 
     protected abstract AccountBox getAccountBox();
 }
