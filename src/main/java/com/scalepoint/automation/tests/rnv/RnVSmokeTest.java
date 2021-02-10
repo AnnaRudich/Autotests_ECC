8f7bf1d01d798ff9669813484ba06aecfc90d8f7
Bartosz Najtkowski <bna@scalepoint.com>
2021-02-10 19:28:38 +0100

    review fixes part3

--- src/main/java/com/scalepoint/automation/tests/rnv/RnVSmokeTest.java
+++ src/main/java/com/scalepoint/automation/tests/rnv/RnVSmokeTest.java
@@ -8,12 +8,14 @@ import com.scalepoint.automation.pageobjects.pages.rnv.ProjectsPage;
 import com.scalepoint.automation.pageobjects.pages.rnv.tabs.InvoiceTab;
 import com.scalepoint.automation.pageobjects.pages.rnv.tabs.OverviewTab;
 import com.scalepoint.automation.pageobjects.pages.suppliers.DefaultSettingsPage;
+import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
 import com.scalepoint.automation.services.restService.RnvService;
 import com.scalepoint.automation.stubs.RnVMock;
 import com.scalepoint.automation.tests.BaseTest;
 import com.scalepoint.automation.utils.Constants;
 import com.scalepoint.automation.utils.NumberFormatUtils;
 import com.scalepoint.automation.utils.RandomUtils;
+import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
 import com.scalepoint.automation.utils.data.entity.input.Claim;
 import com.scalepoint.automation.utils.data.entity.input.ServiceAgreement;
@@ -219,7 +221,8 @@ public class RnVSmokeTest extends BaseTest {
         ProjectsPage projectsPage = new ClaimNavigationMenu().toRepairValuationProjectsPage();
         verifyInvoicesDetails(projectsPage, 1, 0, invoicePrice, invoicePrice);
     }
-
+    @RequiredSetting(type = FTSetting.ENABLE_AUTOMATIC_RV_INVOICE_PAYMENT)
+    @RequiredSetting(type = FTSetting.DEFAULT_AUTOMATIC_INVOICE_PAYMENTS, value = "Insurance company")
     @Test(dataProvider = "testDataProvider", description = "RnV1. SendLine to RnV, send Service Partner feedback")
     public void selfRiskLowerThanTotalAmounOfCompensation(User user, Claim claim, ServiceAgreement agreement, Translations translations) {
 
@@ -259,7 +262,8 @@ public class RnVSmokeTest extends BaseTest {
                 selfRiskTakenByInsureanceCompany,
                 zero);
     }
-
+    @RequiredSetting(type = FTSetting.ENABLE_AUTOMATIC_RV_INVOICE_PAYMENT)
+    @RequiredSetting(type = FTSetting.DEFAULT_AUTOMATIC_INVOICE_PAYMENTS, value = "Insurance company")
     @Test(dataProvider = "testDataProvider", description = "RnV1. SendLine to RnV, send Service Partner feedback")
     public void selfRiskEqualToTotalAmountOfCompensation(User user, Claim claim, ServiceAgreement agreement, Translations translations) {
 
@@ -299,6 +303,8 @@ public class RnVSmokeTest extends BaseTest {
                 selfRiskTakenByInsureanceCompany,
                 zero);
     }
+    @RequiredSetting(type = FTSetting.ENABLE_AUTOMATIC_RV_INVOICE_PAYMENT)
+    @RequiredSetting(type = FTSetting.DEFAULT_AUTOMATIC_INVOICE_PAYMENTS, value = "Insurance company")
     @Test(dataProvider = "testDataProvider", description = "RnV1. SendLine to RnV, send Service Partner feedback")
     public void selfRiskHigherThanTotalAmountOfCompensation(User user, Claim claim, ServiceAgreement agreement, Translations translations) {
 
