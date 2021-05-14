49d443c979ac4e35d7f643eead5f6e75060feb22
Anna Rudich <aru@scalepoint.com>
2021-05-14 13:28:24 +0200

    adding sql procedure to manipulate FeatureToggle setting before tests start. Enabling Reduction rules delay for test purposes

---
+++ src/liquibase/procedures/autotests_manipulate_FeatureToggle_setting.sql
@@ -0,0 +1,24 @@
+--liquibase formatted sql
+--changeset aru:1 runOnChange:true endDelimiter:GO
+
+IF EXISTS (SELECT * FROM dbo.sysobjects WHERE id = object_id(N'[dbo].[autotests_manipulate_FeatureToggle_setting]') AND OBJECTPROPERTY(id, N'IsProcedure') = 1)
+	DROP PROCEDURE [dbo].[autotests_manipulate_FeatureToggle_setting]
+GO
+
+CREATE PROCEDURE [dbo].[autotests_manipulate_FeatureToggle_setting]
+@FeatureToggleUID varchar(100),
+@ToggleState int
+
+AS
+
+SET NONCOUNT ON
+
+
+update [FF4J_FEATURES]
+    set [ENABLE] = @ToggleState
+    where FEAT_UID=@FeatureToggleUID
+
+	SET NOCOUNT OFF
+
+
+--rollback DROP FUNCTION [dbo].[autotests_manipulate_FeatureToggle_setting]
