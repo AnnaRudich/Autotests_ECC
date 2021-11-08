d8288deeddf5d58988afbc8a475e9fc804e16fb1
Bartosz Najtkowski <bna@scalepoint.com>
2021-11-08 22:35:58 +0100

    fixed users attributes

--- src/main/java/com/scalepoint/automation/tests/CompleteClaimExternally.java
+++ src/main/java/com/scalepoint/automation/tests/CompleteClaimExternally.java
@@ -1,8 +1,10 @@
 package com.scalepoint.automation.tests;
 
 import com.scalepoint.automation.services.externalapi.ftemplates.FTSetting;
+import com.scalepoint.automation.services.usersmanagement.CompanyCode;
 import com.scalepoint.automation.testGroups.TestGroups;
 import com.scalepoint.automation.utils.annotations.Jira;
+import com.scalepoint.automation.utils.annotations.UserAttributes;
 import com.scalepoint.automation.utils.annotations.ftoggle.FeatureToggleSetting;
 import com.scalepoint.automation.utils.annotations.functemplate.RequiredSetting;
 import com.scalepoint.automation.utils.data.entity.credentials.User;
@@ -60,7 +62,7 @@ public class CompleteClaimExternally extends BaseTest {
     @Test(groups = {TestGroups.COMPLETE_CLAIM_EXTERNALLY, TestGroups.SCALEPOINT_ID}, dataProvider = "testDataProvider",
             description = "CHARLIE-515 Completing of claim Externally (External email)" +
                     "Possible to Complete claim Externally from Settlement page")
-    public void completeClaimExternallyFromSettlementPageScalepointIdTest(User user, Claim claim) {
+    public void completeClaimExternallyFromSettlementPageScalepointIdTest(@UserAttributes(type = User.UserType.SCALEPOINT_ID, company = CompanyCode.FUTURE) User user, Claim claim) {
         completeClaimExternallyFromSettlementPageTest(user, claim);
     }
 }
