4874143879dbb87aa93994054836ad3061f40b16
Bartosz Najtkowski <bna@scalepoint.com>
2021-12-07 14:34:57 +0100

    test

---
+++ src/main/java/com/scalepoint/automation/services/restService/AdminService.java
@@ -0,0 +1,94 @@
+package com.scalepoint.automation.services.restService;
+
+import com.scalepoint.automation.utils.data.entity.credentials.User;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.scalepoint.automation.utils.Configuration.getEccUrl;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AdminService extends LoginProcessService {
+
+    public AdminService login(User user) {
+
+        super.login(user);
+        return this;
+    }
+
+    public AdminService reqouestOutputManagement() {
+
+        getRequest("/rest/om-admin/status");
+
+        return this;
+    }
+
+    public AdminService requestInsuranceCompanies() {
+
+        getRequest("/rest/ExternalClaimOperation/getInsuranceCompanies.json");
+
+        return this;
+    }
+
+    public AdminService requestOutstandingAmounts() {
+
+        getRequest("/admin/outstanding-amounts/list.json");
+
+        return this;
+    }
+
+    public AdminService getListOfInsuranceCompaniesTest() {
+
+        getRequest("/rest/admin/reductionRules/companies");
+
+        return this;
+    }
+
+    public void getRequest(String uri){
+
+        RequestSpecification requestSpecification = given()
+                .baseUri(getEccUrl())
+                .redirects().follow(false);
+
+        String eccSessionId = data.getEccSessionId();
+
+        if(Optional.ofNullable(eccSessionId).isPresent()) {
+
+            requestSpecification
+                    .sessionId(data.getEccSessionId());
+        }
+
+        response = requestSpecification
+                .get(uri)
+                .then()
+                .extract().response();
+    }
+
+    public AdminService doAssert(Consumer<Asserts> assertsFunc) {
+
+        assertsFunc.accept(new Asserts());
+        return AdminService.this;
+    }
+
+    public class Asserts {
+
+        public Asserts assertAuthorized() {
+
+            assertThat(response.getStatusCode())
+                    .as("Request is unauthorized")
+                    .isEqualTo(HttpStatus.SC_OK);
+            return this;
+        }
+
+        public Asserts assertUnauthorized() {
+
+            assertThat(response.getStatusCode())
+                    .as("Request is should be authorized")
+                    .isEqualTo(HttpStatus.SC_UNAUTHORIZED);
+            return this;
+        }
+    }
+}
