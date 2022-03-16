0f6cda74cba5e6d238faee3edbed2e5b1d7c6f61
bna <bna@scalepoint.com>
2022-03-16 20:00:37 +0100

    before method retry

--- src/main/java/com/scalepoint/automation/utils/testng/RetryListener.java
+++ src/main/java/com/scalepoint/automation/utils/testng/RetryListener.java
@@ -1,6 +1,9 @@
 package com.scalepoint.automation.utils.testng;
 
 import org.testng.IAnnotationTransformer;
+import org.testng.IConfigurable;
+import org.testng.IConfigureCallBack;
+import org.testng.ITestResult;
 import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
@@ -9,11 +12,29 @@ import java.lang.reflect.Method;
 /**
  * Created by bza on 1/16/2018.
  */
-public class RetryListener implements IAnnotationTransformer{
+public class RetryListener implements IAnnotationTransformer, IConfigurable {
 
     @Override
     public void transform(ITestAnnotation iTestAnnotation, Class aClass, Constructor constructor, Method method) {
 
         iTestAnnotation.setRetryAnalyzer(Retrier.class);
+    }
+
+    @Override
+    public void run(IConfigureCallBack iConfigureCallBack, ITestResult iTestResult) {
+
+        iConfigureCallBack.runConfigurationMethod(iTestResult);
+
+        if (iTestResult.getThrowable() != null) {
+
+            for (int i = 0; i <= 3; i++) {
+
+                iConfigureCallBack.runConfigurationMethod(iTestResult);
+                if (iTestResult.getThrowable() == null) {
+
+                    break;
+                }
+            }
+        }
     }
 }
