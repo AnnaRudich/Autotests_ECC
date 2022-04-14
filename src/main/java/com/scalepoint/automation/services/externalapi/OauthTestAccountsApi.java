9c61dbec68c2fee82e6ed8d14d8595f19939afca
bna <bna@scalepoint.com>
2022-04-14 19:33:01 +0200

    try catch for UnmarshalException in oauth

--- src/main/java/com/scalepoint/automation/services/externalapi/OauthTestAccountsApi.java
+++ src/main/java/com/scalepoint/automation/services/externalapi/OauthTestAccountsApi.java
@@ -1,32 +1,50 @@
 package com.scalepoint.automation.services.externalapi;
 
+import com.scalepoint.automation.utils.RandomUtils;
 import com.scalepoint.automation.utils.data.response.Token;
 import org.apache.http.HttpStatus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static io.restassured.RestAssured.given;
 
 public class OauthTestAccountsApi {
 
+    private static Logger logger = LogManager.getLogger(OauthTestAccountsApi.class);
+
     private Token token;
 
     public OauthTestAccountsApi sendRequest() {
+
         return sendRequest(Scope.CASE_INTEGRATION);
     }
 
     public OauthTestAccountsApi sendRequest(Scope scope) {
-        this.token = given().baseUri("https://test-accounts.scalepoint.com").basePath("/connect/token").log().all()
-                .formParam("grant_type", "client_credentials")
-                .formParam("client_id", "test_integration_all_tenants")
-                .formParam("client_secret", "GXUF5XZ1f0TYzIRzJKP483LuxqtKxc9tVDf-k17iPaU")
-                .formParam("scope", scope.getScope())
-                .formParam("tenantId")
-                .when()
-                .post()
-                .then().log().all().statusCode(HttpStatus.SC_OK).extract().as(Token.class);
+
+        try {
+
+            this.token = given().baseUri("https://test-accounts.scalepoint.com").basePath("/connect/token")
+                    .formParam("grant_type", "client_credentials")
+                    .formParam("client_id", "test_integration_all_tenants")
+                    .formParam("client_secret", "GXUF5XZ1f0TYzIRzJKP483LuxqtKxc9tVDf-k17iPaU")
+                    .formParam("scope", scope.getScope())
+                    .formParam("tenantId")
+                    .when()
+                    .post()
+                    .then().log().all()
+                    .statusCode(HttpStatus.SC_OK)
+                    .extract().as(Token.class);
+        }catch (Exception e){
+
+
+            logger.warn("Request failed, randomDelay {} : {}", RandomUtils.randomDelay(1,5), e);
+        }
+
         return this;
     }
 
     public OauthTestAccountsApi sendRequest(Scope scope, String clientId, String clientSecret) {
+
         this.token = given().baseUri("https://test-accounts.scalepoint.com").basePath("/connect/token")
                 .formParam("grant_type", "client_credentials")
                 .formParam("client_id", clientId)
@@ -35,20 +53,24 @@ public class OauthTestAccountsApi {
                 .when()
                 .post()
                 .then()
-                .statusCode(HttpStatus.SC_OK).extract().as(Token.class);
+                .statusCode(HttpStatus.SC_OK)
+                .extract().as(Token.class);
         return this;
     }
 
     public Token getToken() {
+
         return this.token;
     }
 
     public OauthTestAccountsApi setToken(Token token) {
+
         this.token = token;
         return this;
     }
 
     public enum Scope {
+
         CASE_INTEGRATION("case_integration"),
         AUDIT_REPORT_CREATE("audit-report:create"),
         EVENTS_INTERNAL("events-internal"),
@@ -60,12 +82,13 @@ public class OauthTestAccountsApi {
         private String scope;
 
         Scope(String scope) {
+
             this.scope = scope;
         }
 
         public String getScope() {
+
             return scope;
         }
     }
-
 }
